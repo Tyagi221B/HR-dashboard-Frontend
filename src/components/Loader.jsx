15a2efc2e4437c12401d9473b2907a16af907bb5
Asmit tyagi <ashmittyagigms@gmail.com>
2025-03-08 13:31:46 +0530

    Add message display to Loader component after a delay

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,17 @@
+import React, { useState, useEffect } from 'react';
 import "../css/Loader.css";
 
 const Loader = () => {
+  const [showMessage, setShowMessage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMessage(true);
+    }, 5000); 
+
+    return () => clearTimeout(timer); 
+  }, []);
+
   return (
     <div className="loader-container">
       <div className="dots-loader">
@@ -9,6 +20,7 @@ const Loader = () => {
         <span></span>
       </div>
       <p>Loading, please wait...</p>
+      <p>{showMessage ? "Our server was having a snooze, but it's waking up now!" : "We're brewing some magic, hang tight!"}</p>
     </div>
   );
 };
